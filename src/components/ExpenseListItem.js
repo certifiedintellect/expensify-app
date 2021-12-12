60f7c8862c1f1891ae91b3843d4609aa584b469f
nikhil <nikhilmara123@gmail.com>
2021-12-12 17:03:06 +0530

    adding numeraljs and moment to expense list item

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import moment from 'moment';
+import numeral from "numeral";
+
 const ExpenseListItem = (props) => {
   const { description, createdAt, amount, id } = props.expense;
   return (
@@ -9,7 +12,7 @@ const ExpenseListItem = (props) => {
         <h3>{description}</h3>
       </Link>
       <p>
-        {amount} - {createdAt}
+        {numeral(amount/100).format('$0,0.00')} - {moment(createdAt).format('MMMM Do, YYYY')}
       </p>
     </div>
   );
