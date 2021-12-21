073d516299687923e06e74f6a71f9fc9050f8e2b
nikhil <nikhilmara123@gmail.com>
2021-12-21 20:31:40 +0530

    moving header component only to private routes

--- src/routers/PrivateRoute.js
+++ src/routers/PrivateRoute.js
@@ -1,23 +1,32 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Redirect, Route } from "react-router-dom";
+import Header from "../components/Header";
 
 export const PrivateRoute = ({
-    isAuthenticated,
-    component: Component,
-    ...rest 
+  isAuthenticated,
+  component: Component,
+  ...rest
 }) => (
-   <Route {...rest} component={(props) => (
-       isAuthenticated ?
-       <Component {...props}/> :
-       <Redirect to="/" />
-   )}/>
-)
+  <Route
+    {...rest}
+    component={(props) =>
+      isAuthenticated ? (
+        <div>
+          <Header />
+          <Component {...props} />
+        </div>
+      ) : (
+        <Redirect to="/" />
+      )
+    }
+  />
+);
 
 const mapStateToProps = (state) => {
-    return {
-        isAuthenticated: !!state.auth.uid
-    }
-}
+  return {
+    isAuthenticated: !!state.auth.uid,
+  };
+};
 
 export default connect(mapStateToProps)(PrivateRoute);
